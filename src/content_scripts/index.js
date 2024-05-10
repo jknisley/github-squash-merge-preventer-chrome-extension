a36c1224b9545603f94afecb466af6fc59a06838
Jason Knisley <jknisley@appomni.com>
2024-05-10 09:43:53 -0600

    Ensure merge commit option is selected and re-disable squash buttons when the override checkbox is checked

--- src/content_scripts/index.js
+++ src/content_scripts/index.js
@@ -1,12 +1,36 @@
+function disableSquashButtons() {
+    const squashButton = document.querySelector("button.js-merge-box-button-squash");
+    if (squashButton) {
+        squashButton.disabled = true;
+    }
+    
+    const squashButtonGroup = document.querySelector("button.btn-group-squash");
+    if (squashButtonGroup) {
+        squashButtonGroup.disabled = true;
+    }
+}
+
 function checkBranch(){
     chrome.storage.sync.get(({branchesList:[]}), (data)=>{  
         const targetBranch = document.querySelector("span.commit-ref span.css-truncate-target").innerText
-        if (data.branchesList.includes(targetBranch)){
-            const button = document.querySelector("button.js-merge-box-button-squash")
-            if (button){
-                button.disabled=true
-            }else{
-                setTimeout(checkBranch,125)
+        if (data.branchesList.includes(targetBranch)) {
+            // ensure the merge commit option is selected
+            const mergeButton = document.querySelector("button.js-merge-box-button-merge");
+            if (mergeButton) {
+                mergeButton.click();
+            }
+
+            // disabled buttons to select squash and merge
+            disableSquashButtons();
+
+            // when the override checkbox is checked, we need to re-disable the squash buttons
+            const mergeOverrideCheckbox = document.querySelector("input[type='checkbox'].js-admin-merge-override");
+            if (mergeOverrideCheckbox) {
+                mergeOverrideCheckbox.addEventListener('change', function() {
+                    if (this.checked) {
+                        setTimeout(disableSquashButtons, 125);
+                    }
+                });
             }
         }    
     })
@@ -14,5 +38,5 @@ function checkBranch(){
 
 
 window.onload = function() {
-    setTimeout(checkBranch, 125)
+    setTimeout(checkBranch, 1000);
 }
